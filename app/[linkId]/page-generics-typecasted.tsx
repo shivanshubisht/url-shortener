d1e5333ee627ddb9a6b357ebf82d9c898d7a96d6
shivanshubisht <77038395+shivanshubisht@users.noreply.github.com>
2023-01-14 03:16:23 +0530

    Add files via upload

---
+++ app/[linkId]/page-generics-typecasted.tsx
@@ -0,0 +1,39 @@
+import { redirect } from 'next/navigation';
+import Redirect from '../../components/Redirect';
+import { prisma } from '../../server/db/client';
+
+interface ParamsType {
+  params: { linkId: string };
+  searchParams: { [key: string]: string };
+}
+
+function asyncComponent<T, R>(
+  fn: (arg: T) => Promise<R | null>
+): (arg: T) => R {
+  return fn as (arg: T) => R;
+}
+
+const Link = asyncComponent<ParamsType, JSX.Element>(async ({ params }) => {
+  const url = await prisma.link.findFirst({
+    where: {
+      OR: [
+        {
+          customname: params.linkId,
+        },
+        {
+          linkId: params.linkId,
+        },
+      ],
+    },
+  });
+  if (url) {
+    {
+      console.log(url.url);
+      return url ? <Redirect url={url.url} /> : null;
+    }
+    // redirect(url.url);
+  }
+  return <div>URL not found</div>;
+});
+
+export default Link;
